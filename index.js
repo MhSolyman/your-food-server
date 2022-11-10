03e8b7a008d3f58fa6d82492bcb4bb99234b6f1d
MhSolyman <solymanbijoye.com>
2022-11-10 19:35:28 +0600

    add env

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const cors = require('cors');
 app.use(cors());
 app.use(express.json())
 
 const port = process.env.PORT || 5000;
+console.log(process.env.DB_USER)
 
 
 
 
-
-const uri = "mongodb+srv://MhSolyman:S1yZIov9LhidqeAZ@cluster0.kmh2g.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kmh2g.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
