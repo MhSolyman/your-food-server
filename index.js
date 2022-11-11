c138acc97d18e77aa63329854602379b15d4e2ab
MhSolyman <solymanbijoye.com>
2022-11-11 23:10:12 +0600

    add lust data

--- index.js
+++ index.js
@@ -26,28 +26,76 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const run = async () => {
     try {
         const foodCullection = client.db('food').collection('foods');
-        app.get('/food', async(req, res) => {
+        const reviewsCullectio = client.db('review').collection('reviews');
+        app.get('/food', async (req, res) => {
             const query = {};
-            const cursor= foodCullection.find(query).limit(3)
+            const cursor = foodCullection.find(query).limit(3)
             const food = await cursor.toArray();
             res.send(food)
         })
 
-        const foodCullections = client.db('food').collection('foods');
-        app.get('/food/services', async(req, res) => {
+
+        app.get('/food/services', async (req, res) => {
             const query = {};
-            const cursor= foodCullection.find(query)
+            const cursor = foodCullection.find(query)
             const food = await cursor.toArray();
             res.send(food)
         })
 
-        app.get('/food/services/:id', async(req, res) => {
+        app.get('/food/services/:id', async (req, res) => {
             const id = req.params.id;
 
             const query = { _id: ObjectId(id) };
             const foods = await foodCullection.findOne(query)
             res.send(foods)
         })
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCullectio.insertOne(review);
+            res.send(result)
+
+        })
+        app.post('/food/services', async (req, res) => {
+            const review = req.body;
+            const result = await foodCullection.insertOne(review);
+            res.send(result)
+
+        })
+        app.get('/reviews', async (req, res) => {
+
+            let query = {};
+            if (req.query.service) {
+                query = {
+                    service: req.query.service
+                }
+            }
+
+            const cursor = reviewsCullectio.find(query);
+            const revew = await cursor.toArray()
+            res.send(revew)
+
+        }
+        )
+
+
+        app.get('/Addreviews', async (req, res) => {
+
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+
+            const cursor = reviewsCullectio.find(query);
+            const revew = await cursor.toArray()
+            res.send(revew)
+
+        }
+        )
+
+
+
 
     }
     finally {
@@ -60,7 +108,7 @@ run().catch(error => console.log(error));
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
 })
 
